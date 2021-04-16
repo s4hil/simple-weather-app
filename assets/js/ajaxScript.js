3f223dd240c0396bbf87d58bd7994c185515f814
s4hil <80034527+s4hil@users.noreply.github.com>
2021-04-16 14:56:39 +0530

    Add files via upload

---
+++ assets/js/ajaxScript.js
@@ -0,0 +1,54 @@
+console.log('Script Loaded');
+
+
+$('#searchBtn').click( (e) => {
+	e.preventDefault();
+
+	// Getting Labels
+	var cityName = $('#city').val();
+	var city = $('.place');
+	var temp = $('.temp');
+	var weather = $('.weatherCondition');
+	var weatherDes = $('.weatherDes');
+	var date = $('.date');
+	var icon = $('.icon');
+
+
+	var cityName = cityName.toLowerCase();
+	var token = "5f6aaa2f7afc7ec45bbbfe66d0d2457d";
+	var url = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+token+"";
+
+	var cardTitle = $('.card-title');
+
+	$.ajax({
+		url: url,
+		method: "post",
+		responseType: "json",
+		success: function (data) {
+			if (data.cod === 200) {
+				$('.card-box').fadeIn(1000);
+				city.html(data.name);
+				weather.html(data.weather[0].main);
+				weatherDes.html(data.weather[0].description);
+				icon.attr("src", fetchIcon(data.weather[0].icon));
+				temp.html(formatTemp(data.main.temp));
+			}
+		},
+		error: function () {
+			alert('City Not Found');
+		},
+	});
+
+	// Converting F to C
+	function formatTemp(temp) {
+		tempC = Number(temp) - 273;
+		tempC = tempC.toFixed(2) + "&deg;" ;
+		return tempC;
+	}
+
+	// Fetching icon URL
+	function fetchIcon(code) {
+		var iconURL = "http://openweathermap.org/img/w/" + code + ".png";
+		return iconURL;
+	}
+});
